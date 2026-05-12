bac60a1ad29d97f9aedc1a63d96902c6630a12e3
Vincent Ogweno <ogwenovincent00@gmail.com>
2026-05-12 22:06:05 +0300

    implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import AdminLayout from "./layouts/AdminLayout";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import ManageServices from "./pages/admin/ManageServices";
-import ManageBookings from "./pages/admin/ManageBookings";
-import ManageClients from "./pages/admin/ManageClients";
 import { Suspense, lazy } from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// 🔥 Lazy imports
+// Shared
 const Home = lazy(() => import("./pages/shared/Home"));
 const Signup = lazy(() => import("./pages/shared/Signup"));
 const Login = lazy(() => import("./pages/auth/Login"));
 
+// Client
 const ClientLayout = lazy(() => import("./layouts/ClientLayout"));
 const Dashboard = lazy(() => import("./pages/client/Dashboard"));
-import Profile from "./pages/client/Profile";
-
+const Profile = lazy(() => import("./pages/client/Profile"));
 const Services = lazy(() => import("./pages/client/Services"));
 const BookAppointment = lazy(() => import("./pages/client/BookAppointment"));
 const MyAppointments = lazy(() => import("./pages/client/MyAppointments"));
 const ServicesHistory = lazy(() => import("./pages/client/MyServicesHistory"));
 const Notifications = lazy(() => import("./pages/client/Notifications"));
 const Payment = lazy(() => import("./pages/client/Payments"));
+
+// Admin
+const AdminLayout = lazy(() => import("./layouts/AdminLayout"));
+const AdminDashboard = lazy(() => import("./pages/admin/AdminDashboard"));
+const ManageServices = lazy(() => import("./pages/admin/ManageServices"));
+const ManageBookings = lazy(() => import("./pages/admin/ManageBookings"));
+const ManageClients = lazy(() => import("./pages/admin/ManageClients"));
 
 function App() {
   return (
